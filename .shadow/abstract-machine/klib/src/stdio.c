f3b3816bf5d21b650e3255e692f30b5dfe8df9d1
tracer-nju <tracer@nju.edu.cn>
2023-04-08 20:06:27 +0800

    Linux Kegemo 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:06:27 up 51 min,  0 users,  load average: 0.03, 0.01, 0.00

--- .shadow/abstract-machine/klib/src/stdio.c
+++ .shadow/abstract-machine/klib/src/stdio.c
@@ -6,8 +6,44 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+int pow(int x, int y) {
+  if(y == 0) return 1;
+  return pow(x, y-1) * x; 
+}
+
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  char *str = (char *) fmt;
+  va_list ap;
+  va_start(ap, fmt);
+  int d;
+  while(*str != '\0') {
+    switch (*str++) {
+      case '%':
+        str++;
+        switch (*str) {
+          case 'd':
+            d = va_arg(ap, int);
+            int len = 0, tmp = d;
+            while(tmp / 10) {
+              len++;
+              tmp = tmp / 10;
+            }
+            char ch;
+            while(len) {
+              ch = (d / pow(10, len)) % 10 + '0';
+              putch(ch);
+              d = d % pow(10, len);
+              len--;
+            }
+            continue;
+        }
+        default: putch(*str); continue;
+      }
+      
+  }
+  va_end(ap);
+  // panic("Not implemented");
+  return 0;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -61,7 +61,9 @@ int main(const char *args) {
   puts("\"\n");
 
   splash();
-
+  int width = io_read(AM_GPU_CONFIG).width;
+  // int height = io_read(AM_GPU_CONFIG).height;
+  printf("%d", width);
   puts("Press any key to see its key code...\n");
   while (1) {
     print_key();
