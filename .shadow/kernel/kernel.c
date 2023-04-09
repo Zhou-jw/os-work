07e8ab2971f32a73bfe53ab581abe47aa9f9e44a
tracer-nju <tracer@nju.edu.cn>
2023-04-09 14:47:34 +0800

    Linux Kegemo 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:47:33 up 19:00,  0 users,  load average: 0.51, 0.18, 0.11

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -52,19 +52,47 @@ void splash() {
   }
 }
 
-void read_pic_wh(char *str, int *pic_w, int *pic_h) {
+int read_pic_wh(char *str, int *pic_w, int *pic_h) {
   // read pic_w
-  *pic_w = atoi(str + 3);
+  char *tmp = str;
+  *pic_h = atoi(str + 3);
   str = str + 3;
   while(*str != ' ')
     str++;
-  *pic_h = atoi(str+1);
+  *pic_w = atoi(str+1);
+  str++;
+  putch(*str);
+  return str+4-tmp;
 }
+
+// static void draw(int x, int y, int w, int h, uint32_t color){
+//   uint32_t pixels[w * h]; // WARNING: large stack-allocated memory
+//   AM_GPU_FBDRAW_T event = {
+//     .x = x, .y = y, .w = w, .h = h, .sync = 1,
+//     .pixels = pixels,
+//   };
+//   for (int i = 0; i < w * h; i++) {
+//     pixels[i] = color;
+//   }
+//   ioe_write(AM_GPU_FBDRAW, &event);
+// }
+// int get_color() {
+//   int color
+// }
+// void disp_xy2uv(int pic_w, int pic_h, int width, int height,char *str) {
+//   // display xy to uv with color (pic_h, pic_w) -> (width, height)
+//   for(int x=0; x<pic_h; x++) {
+//     for(int y=0; y<pic_w; y++) {
+//       u = x * height / pic_h;
+//       v = y * width / pic_w;
+//       draw(u, v, 1, 1, color);
+//     }
+//   }
+// }
 
 // Operating system is a C program!
 int main(const char *args) {
   ioe_init();
-
   puts("mainargs = \"");
   puts(args);  // make run mainargs=xxx
   puts("\"\n");
@@ -73,8 +101,8 @@ int main(const char *args) {
   // int width = io_read(AM_GPU_CONFIG).width;
   // int height = io_read(AM_GPU_CONFIG).height;
   int pic_w, pic_h;
-  read_pic_wh(photop3_ppm, &pic_w, &pic_h);
-  printf("%d %d", pic_w, pic_h);
+  int color_start = read_pic_wh(photop3_ppm, &pic_h, &pic_w);
+  printf("%d %d %d", pic_w, pic_h, color_start);
   puts("Press any key to see its key code...\n");
   while (1) {
     print_key();
